baca276d8fe83683bba7ae986812715d1743ed0d
grimza99 <gbtmxlf@naver.com>
2025-11-14 19:20:01 +0900

    feat: ArrowContainer 컴포넌트 및 화살표 아이콘 추가

---
+++ app/(test-page-layout)/_testpage/components/arrow-container/index.tsx
@@ -0,0 +1,18 @@
+interface ArrowContainerProps {
+  title: string;
+  children: React.ReactNode;
+}
+export default function ArrowContainer({
+  title,
+  children,
+}: ArrowContainerProps) {
+  return (
+    <>
+      <button className="flex items-center gap-2 mb-8 text-[24.5px] lg:text-[28px]">
+        <h2>{title}</h2>
+        <img src="/icons/black_arrow_right.icon.svg" />
+      </button>
+      {children}
+    </>
+  );
+}
