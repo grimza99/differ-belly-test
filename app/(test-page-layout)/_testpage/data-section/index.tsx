a3ef43c2e53331f908f8b4e3a81927606b5133d7
grimza99 <gbtmxlf@naver.com>
2025-11-14 19:12:00 +0900

    feat: data section의 카드 스타일링

---
+++ app/(test-page-layout)/_testpage/data-section/index.tsx
@@ -0,0 +1,18 @@
+export default function DataSection() {
+  return (
+    <section className="px-5 lg:px-0">
+      <h2>데이터</h2>
+      <button className="bg-purple600 rounded-2xl flex flex-col items-center gap-8 w-full">
+        <img
+          src="/icons/mail.icon.svg"
+          alt="mail.icon.svg"
+          className="w-[118px] mt-[54px]"
+        />
+        <p className="overflow-wrap text-white text-center mb-[47px]">
+          야놀자 리서치가 활용한 데이터를
+          <br /> 편하게 사용해보세요!
+        </p>
+      </button>
+    </section>
+  );
+}
