bfb3b2982907b5da382a638529c38982335416fa
grimza99 <gbtmxlf@naver.com>
2025-11-14 15:07:54 +0900

    feat: Nav 컴포넌트 추가 및 데스크탑/비데스크탑 내비게이션 항목 설정

---
+++ app/(test-page-layout)/_testpage/components/header/Nav.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+const desktopNavItems = [
+  { name: "Research", path: "/" },
+  { name: "Datalab", path: "/datalab" },
+  { name: "Indexes", path: "/indexes" },
+  { name: "About", path: "/about" },
+];
+const nonDesktopNavItems = [
+  { name: "HOME", path: "/" },
+  { name: "RESEARCH", path: "/" },
+  { name: "INDEXES", path: "/indexes" },
+  { name: "ABOUT", path: "/about" },
+];
+export default function Nav({ isSideHeader }: { isSideHeader?: boolean }) {
+  return (
+    <nav
+      className={clsx(
+        isSideHeader && "flex flex-col text-[28px] gap-10",
+        !isSideHeader && "items-center w-full text-center flex"
+      )}
+    >
+      {!isSideHeader
+        ? desktopNavItems.map((item) => (
+            <Link key={item.name} href={item.path} className="flex-1">
+              {item.name}
+            </Link>
+          ))
+        : nonDesktopNavItems.map((item) => (
+            <Link key={item.name} href={item.path} className="flex-1">
+              {item.name}
+            </Link>
+          ))}
+    </nav>
+  );
+}
