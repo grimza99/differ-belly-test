6d69280067dd83db5fe1f53fa0bf79033360720e
grimza99 <gbtmxlf@naver.com>
2025-11-18 13:14:01 +0900

    feat: ArrowButton 컴포넌트 클릭 이벤트 추가

--- app/_common/arrow-button/ArrowButton.tsx
+++ app/_common/arrow-button/ArrowButton.tsx
@@ -1,15 +1,28 @@
+import clsx from "clsx";
+
 export default function ArrowButton({
   direction,
+  className,
+  onClick,
 }: {
   direction: "left" | "right";
+  onClick: () => void;
+
+  className?: string;
 }) {
   return (
-    <div className="hidden lg:flex w-5 h-5 bg-white rounded-full items-center justify-center">
+    <button
+      className={clsx(
+        "hidden lg:flex bg-white rounded-full items-center justify-center p-[8.5px] w-[37px] h-[37px] ",
+        className
+      )}
+      onClick={onClick}
+    >
       {direction === "left" ? (
         <img src="/icons/gray_arrow_left.icon.svg" />
       ) : (
         <img src="/icons/gray_arrow_right.icon.svg" />
       )}
-    </div>
+    </button>
   );
 }
