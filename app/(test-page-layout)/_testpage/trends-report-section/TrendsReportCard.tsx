7ba62bc72024eed6c124668fbf9a27323ca30317
grimza99 <gbtmxlf@naver.com>
2025-11-18 13:58:21 +0900

    feat: TrendsReportCard 컴포넌트 추가 및 이미지 파일 추가

---
+++ app/(test-page-layout)/_testpage/trends-report-section/TrendsReportCard.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+interface TrendsReportCardProps {
+  src: string;
+  title: string;
+  date: string;
+}
+export default function TrendsReportCard({
+  src,
+  title,
+  date,
+}: TrendsReportCardProps) {
+  return (
+    <div className="relative flex w-47.5 h-[267px] md:w-full md:h-[1049px] lg:w-77 lg:h-108 rounded-2xl overflow-hidden">
+      <Image
+        src={src}
+        alt={src}
+        fill
+        className="absolute inset-0 object-cover rounded-2xl -z-10"
+      />
+      <div className="w-full h-full absolute inset-0 bg-black/40 -z-5 flex flex-col justify-end-safe px-5 py-7.5">
+        <p className="w-full text-white lg:text-xl">{title}</p>
+        <span className="text-white text-xs lg:text-sm">{date}</span>
+      </div>
+    </div>
+  );
+}
