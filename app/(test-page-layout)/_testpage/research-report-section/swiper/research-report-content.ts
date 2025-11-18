277765b776cef31ce1002e64be19572aaedae21c
grimza99 <gbtmxlf@naver.com>
2025-11-18 14:36:50 +0900

    feat: ResearchReportCard 및 연구 보고서 콘텐츠 추가

---
+++ app/(test-page-layout)/_testpage/research-report-section/swiper/research-report-content.ts
@@ -0,0 +1,27 @@
+export const RESEARCH_REPORT_CONTENT = [
+  {
+    id: 1,
+    src: "/report/img1.jpg",
+    date: "2025.11.17",
+  },
+  {
+    id: 2,
+    src: "/report/img2.png",
+    date: "2025.11.17",
+  },
+  {
+    id: 3,
+    src: "/report/img3.png",
+    date: "2025.11.17",
+  },
+  {
+    id: 4,
+    src: "/report/img4.png",
+    date: "2025.11.17",
+  },
+  {
+    id: 5,
+    src: "/report/img5.png",
+    date: "2025.11.17",
+  },
+];
