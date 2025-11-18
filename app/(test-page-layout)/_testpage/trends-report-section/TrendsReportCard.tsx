2b4e496b951de25512a66e508ae4c9c411657ec9
grimza99 <gbtmxlf@naver.com>
2025-11-18 18:14:49 +0900

    feat: 트렌드 보고서 콘텐츠 추가

--- app/(test-page-layout)/_testpage/trends-report-section/TrendsReportCard.tsx
+++ app/(test-page-layout)/_testpage/trends-report-section/TrendsReportCard.tsx
@@ -11,18 +11,16 @@ export default function TrendsReportCard({
   date,
 }: TrendsReportCardProps) {
   return (
-    <div>
-      <div className="relative flex w-47.5 h-[267px] md:w-187 md:h-[1049px] lg:w-77 lg:h-108 rounded-2xl overflow-hidden">
-        <Image
-          src={src}
-          alt={src}
-          fill
-          className="absolute inset-0 object-cover rounded-2xl -z-10"
-        />
-        <div className="w-full h-full absolute inset-0 bg-black/40 -z-5 flex flex-col justify-end-safe px-5 py-7.5">
-          <p className="w-full text-white lg:text-xl">{title}</p>
-          <span className="text-white text-xs lg:text-sm">{date}</span>
-        </div>
+    <div className="relative flex w-47.5 h-[267px] md:w-187 md:h-[1049px] lg:w-77 lg:h-108 rounded-2xl overflow-hidden">
+      <Image
+        src={src}
+        alt={src}
+        fill
+        className="absolute inset-0 object-cover rounded-2xl -z-10"
+      />
+      <div className="w-full h-full absolute inset-0 bg-black/40 -z-5 flex flex-col justify-end-safe px-5 py-7.5">
+        <p className="w-full text-white lg:text-xl">{title}</p>
+        <span className="text-white text-xs lg:text-sm">{date}</span>
       </div>
     </div>
   );
---
+++ app/(test-page-layout)/_testpage/trends-report-section/trends-report-content.ts
@@ -0,0 +1,26 @@
+export const TRENDS_REPORT_CONTENT = [
+  {
+    id: 1,
+    src: "/trends/img1.png",
+    content: "2024년 1분기 국내 숙박업 동향 보고서",
+    date: "2025.11.18",
+  },
+  {
+    id: 2,
+    src: "/trends/img1.png",
+    content: "2024년 2분기 국내 숙박업 동향 보고서",
+    date: "2025.11.18",
+  },
+  {
+    id: 3,
+    src: "/trends/img1.png",
+    content: "2024년 3분기 국내 숙박업 동향 보고서",
+    date: "2025.11.18",
+  },
+  {
+    id: 4,
+    src: "/trends/img1.png",
+    content: "2024년 4분기 국내 숙박업 동향 보고서",
+    date: "2025.11.18",
+  },
+];
