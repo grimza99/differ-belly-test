325d0d31f4ad9e0a6e618d797785a614cf464163
grimza99 <gbtmxlf@naver.com>
2025-11-19 13:21:47 +0900

    feat: 애니메이션 섹션 추가 및 스타일 개선

--- app/(test-page-layout)/page.tsx
+++ app/(test-page-layout)/page.tsx
@@ -1,3 +1,4 @@
+import AnimatedSection from "../_common/AnimatedSection";
 import BriefReportSection from "./_testpage/brief-report-section";
 import DataSection from "./_testpage/data-section";
 import InsightSection from "./_testpage/insight-section";
@@ -9,17 +10,32 @@ import TrendsReportSection from "./_testpage/trends-report-section";
 export default function Home() {
   return (
     <div className="w-full flex flex-col ">
-      <InsightSection />
-      <div className="flex flex-col lg:flex-row mt-13 mb-20">
+      <AnimatedSection animationType="fade-up">
+        <InsightSection />
+      </AnimatedSection>
+      <AnimatedSection
+        animationType="fade-up"
+        className="flex flex-col lg:flex-row mt-13 mb-20"
+      >
         <BriefReportSection />
         <TrendsReportSection />
-      </div>
-      <ResearchReportSection />
-      <div className="flex flex-col gap-14 lg:flex-row lg:gap-20 lg:px-[15px] max-w-5xl w-full m-auto my-14">
+      </AnimatedSection>
+
+      <AnimatedSection animationType="fade-up">
+        <ResearchReportSection />
+      </AnimatedSection>
+
+      <AnimatedSection
+        animationType="fade-up"
+        className="flex flex-col gap-14 lg:flex-row lg:gap-20 lg:px-[15px] max-w-5xl w-full m-auto my-14"
+      >
         <NewsSection />
         <DataSection />
-      </div>
-      <SNSSection />
+      </AnimatedSection>
+
+      <AnimatedSection animationType="fade-up">
+        <SNSSection />
+      </AnimatedSection>
     </div>
   );
 }
